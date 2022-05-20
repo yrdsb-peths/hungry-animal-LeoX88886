12860aae43834d70150a68848091718c1ee13919
Xu <348962257@EDU.YRDSB.NET>
2022-05-20 10:28:36 -0400

    added instructions and saved world

--- TitleScreen.java
+++ TitleScreen.java
@@ -9,7 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class TitleScreen extends World
 {
     Label titleLabel = new Label("Hungery Bear", 50);
-    
+
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -18,10 +18,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, getWidth()/2, 200);
+        prepare();
     }
-    
+
     public void act()
     {
         if (Greenfoot.isKeyDown("space"))
@@ -29,5 +30,33 @@ public class TitleScreen extends World
             MyWorld MyWorld = new MyWorld();
             Greenfoot.setWorld(MyWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Bear bear = new Bear();
+        addObject(bear,431,100);
+        bear.setLocation(298,84);
+
+        Label label = new Label("Press <space_key> to play", 50);
+        addObject(label,292,277);
+        label.setLocation(342,251);
+
+        Label label2 = new Label("Use <WASD> to move", 30);
+        addObject(label2,292,281);
+        label2.setLocation(320,304);
+
+        Label label3 = new Label("Press <C_key> to sprint", 30);
+        addObject(label3,279,344);
+        label3.setLocation(389,234);
+
+        
+        label.setLocation(319,344);
+        label.setLocation(344,242);
+        label2.setLocation(317,306);
     }
 }
